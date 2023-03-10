4e560489cba0f22e5939063537c4c6ef6285d218
yousufislamme <online.okk@gmail.com>
2023-03-10 22:39:23 +0600

    add some math

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
 <?php
-echo "Hello";
+$number = 100;
+$number2 = 20;
+
+// +
+$number += $number2;
+echo $number . "\n";
+
+// -
+$number -= $number2;
+echo $number . "\n";
+// *
+$number *= $number2;
+echo $number . "\n";
+// /
+$number /= $number2;
+echo $number . "\n";
+// %
+$number %= $number2;
+echo $number . "\n";
