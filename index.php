102a4af088884b4dd872e5c106d3ced0768fa20d
yousufislamme <online.okk@gmail.com>
2023-03-10 20:55:00 +0600

    add variable & const & define

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
-echo "Hello";
+
+$name = 'Bangladesh';
+$age = 23;
+$aim = 'Rich man in the World!';
+$newAim = 'aim';
+define('data', 'Yousuf');
+
+// new tricks
+echo $$newAim . "\n";
+echo "this is our {$name}\n";
+
+echo "Hello I'm " . data;
