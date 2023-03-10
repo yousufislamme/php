3b17c5e6c59a25b5b2fceb6f1b0114a24f1bd8da
yousufislamme <online.okk@gmail.com>
2023-03-10 19:10:07 +0600

    use of Array

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
-echo "Hello";
+
+$students = array(
+    "Rohim",
+    "Korim",
+    "Rakib",
+    777,
+    2.0,
+    "okay",
+);
+// count all list...
+$all = count($students);
+
+for ($i = 0; $i < ($all); $i++) {
+    echo $students[$i] . "\n";
+}
