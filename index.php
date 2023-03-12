95f149577a4f6b4d22270665dd66a2036ceb6805
yousufislamme <online.okk@gmail.com>
2023-03-13 00:53:31 +0600

    add some importain conditations

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
 <?php
-$n = 11;
-$Anas = 1000;
-$Aouf = 1000;
+// $n = 11;
+// $Anas = 1000;
+// $Aouf = 1000;
 
-if ($Anas > $Aouf) {
-    echo "Anas er TK beshi...";
-} elseif ($Anas == $Aouf) {
-    echo "Tader TK equal";
+// if ($Anas > $Aouf) {
+//     echo "Anas er TK beshi...";
+// } elseif ($Anas == $Aouf) {
+//     echo "Tader TK equal";
+// } else {
+//     echo "Aouf er TK beshi...";
+
+// }
+
+// $age = 11;
+
+// if ($age >= 13 && $age <= 19) {
+//     echo "this person is Teenager";
+// } else {
+//     echo "this person is not Teenager";
+// }
+
+$food = "orange";
+
+if ("apple" == $food || "mango" == $food) {
+    echo "Here is some {$food}";
 } else {
-    echo "Aouf er TK beshi...";
+    echo "Sorry! we don't have any {$food}";
 
 }
