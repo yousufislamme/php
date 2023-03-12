7e37ecb964d717aa10f16a1b40768ad5459396f9
yousufislamme <online.okk@gmail.com>
2023-03-12 23:07:38 +0600

    add sprintf

--- printf.php
+++ printf.php
@@ -6,5 +6,11 @@ printf('I have two friends 1st %2$s 2nd %1$s', $a, $b);
 echo "\n";
 
 $n = 45.13666;
-
 printf("My PHP point is %.2f", $n);
+echo "\n";
+
+// use sprintf
+
+$output = sprintf('Hello! My name is %2$s %1$s...', $a, $b);
+
+echo $output;
