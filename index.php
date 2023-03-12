d986bd5792a19765d6332705ee95ccc51c4cf4d7
yousufislamme <online.okk@gmail.com>
2023-03-13 00:24:57 +0600

    simple condition

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
-echo "Hello";
+$n = 11;
+$Anas = 1000;
+$Aouf = 1000;
+
+if ($Anas > $Aouf) {
+    echo "Anas er TK beshi...";
+} elseif ($Anas == $Aouf) {
+    echo "Tader TK equal";
+} else {
+    echo "Aouf er TK beshi...";
+
+}
