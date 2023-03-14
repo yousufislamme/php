eaa4a008027b4aeda90cc928490a7e02b5cdd2f7
yousufislamme <online.okk@gmail.com>
2023-03-15 00:21:36 +0600

    add ternary operator

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-echo "Hello";
+
+$n = 24;
+
+$result = ($n % 2 == 0) ? "{$n} is a jor songkha..." : "{$n} is a bijor songkha...";
+
+echo $result;
+
+echo "\n";
+
+$a = 7;
+
+$result2 = $a <= 7 ? "{$a} is a lucky number" : "{$a} is a not lucky number";
+
+echo $result2;
