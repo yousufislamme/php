43ca6b6bf5581f382ca726bc7c243458dc8cb478
yousufislamme <online.okk@gmail.com>
2023-03-14 23:52:41 +0600

    set if-else condition right way

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-echo "Hello";
+
+$condition1 = true;
+$condition2 = false;
+$condition3 = true;
+
+if ($condition1 && $condition2 && $condition3) {
+    echo "condition 1 Hello Bangladesh";
+} elseif ($condition1 && $condition2) {
+    echo "condition 2 ";
+} elseif ($condition2 && $condition3) {
+    echo "condition 3 ";
+} elseif ($condition1 && $condition3) {
+    echo "condition 4";
+}
