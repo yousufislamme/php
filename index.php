504ab18b801bdb8de83abe9bbbe852d6b2cf0788
yousufislamme <online.okk@gmail.com>
2023-03-15 00:38:41 +0600

    I am using how to work Switch method

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
-echo "Hello";
+
+$color = "red";
+switch ($color) {
+    case 'green':
+        echo "Green color is my favorite";
+        break;
+    case 'red':
+        echo "Red color is not my favorite";
+        break;
+    default:
+        echo "All color is ok";
+        break;
+}
