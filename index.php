fd9d7d9bd5b65cab6057b4df5d47095315130cb5
yousufislamme <online.okk@gmail.com>
2023-03-15 10:06:08 +0600

    I am using a condition with SWITCH related phones

--- index.php
+++ index.php
@@ -12,3 +12,23 @@ switch ($color) {
         echo "All color is ok";
         break;
 }
+
+echo "\n";
+
+// I using a small condition...
+$phones = 'samsung';
+
+switch ($phones) {
+    case 'iPhone':
+        echo "iPhone is a most popular & powerful";
+        break;
+    case 'mi':
+        echo "mi is a modern and low price to best device";
+        break;
+    case 'oppo':
+        echo "Oppo is bad phone";
+        break;
+    default:
+        echo "it's maybe good";
+
+}
