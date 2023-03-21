755aed5b40cd12d01f910fb35754a00d61f3013c
yousufislamme <online.okk@gmail.com>
2023-03-21 09:42:09 +0600

    add functions & check isEven conseft

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+// this code checks whether is Even or Odd
+function isEven($number) {
+    if ($number % 2 == 0) {
+        return true;
+    }
+    return false;
+}
--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
+// here it full source is at functions.php
+include_once "functions.php";
+$value = 13;
 
-for ($i = 10, $j = 1; $i > 0; $i--, $j++) {
-    echo $i . ":" . $j;
-    echo PHP_EOL;
+if (isEven($value)) {
+    echo "{$value} is a Even number";
+} else {
+    echo "{$value} is a Odd number";
 }
